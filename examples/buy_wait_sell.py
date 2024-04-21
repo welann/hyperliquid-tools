09c911c96fa8cf3184ec94bb1faa147a7715e89d
welann <wzcing@outlook.com>
2024-04-21 16:07:29 +0800

    刷量代码可以用在 PURR/USDC 上了

--- examples/buy_wait_sell.py
+++ examples/buy_wait_sell.py
@@ -6,6 +6,7 @@ import time
 import random
 
 from hyperliquid.utils.signing import OrderType
+from hyperliquid.utils.types import Any, List, Meta, SpotMeta, Optional, Tuple, Cloid
 
 import logging
 
@@ -13,6 +14,9 @@ logger = logging.getLogger(__name__)
 
 
 class buyandsell:
+    # Default Max Slippage for Market Orders 1%
+    DEFAULT_SLIPPAGE = 0.01
+
     def __init__(
         self, address: str, info: Info, exchange: Exchange, COIN="ETH", buyamount=0.05, waittime=2, isbuy=True
     ):
@@ -23,6 +27,56 @@ class buyandsell:
         self.buyamount = buyamount
         self.waittime = waittime
         self.isbuy = isbuy
+
+    def _slippage_price(
+        self,
+        coin: str,
+        is_buy: bool,
+        slippage: float,
+        px: Optional[float] = None,
+    ) -> float:
+
+        if not px:
+            # Get midprice
+            px = float(self.info.all_mids()[coin])
+        # Calculate Slippage
+        px *= (1 + slippage) if is_buy else (1 - slippage)
+        # We round px to 5 significant figures and 6 decimals
+        return round(float(f"{px:.5g}"), 6)
+
+    def market_close(
+        self,
+        coin: str,
+        sz: Optional[float] = None,
+        px: Optional[float] = None,
+        slippage: float = DEFAULT_SLIPPAGE,
+        cloid: Optional[Cloid] = None,
+    ):
+        address = self.address
+        # if self.address:
+        #     address = self.address
+        # if self.vault_address:
+        #     address = self.vault_address
+        if self.COIN=="PURR/USDC":
+            logger.info("sell purr")
+            px = self._slippage_price(self.COIN, False, slippage)
+            return self.exchange.order(self.COIN,False,self.buyamount,px,{"limit": {"tif": "Gtc"}})
+    
+        positions = self.info.user_state(address)["assetPositions"]
+        for position in positions:
+            item = position["position"]
+            if coin != item["coin"]:
+                continue
+            szi = float(item["szi"])
+            if not sz:
+                sz = abs(szi)
+            is_buy = True if szi < 0 else False
+            
+            # Get aggressive Market Price
+            px = self._slippage_price(coin, is_buy, slippage, px)
+            # Market Order is an aggressive Limit Order IoC
+            return self.exchange.order(coin, is_buy, sz, px, order_type={"limit": {"tif": "Ioc"}} , reduce_only=True, cloid=cloid)
+        
 
     def order(self):
         order_result = self.exchange.market_open(self.COIN, self.isbuy, self.buyamount, None, 0.01)
@@ -38,8 +92,7 @@ class buyandsell:
 
             logger.info(f"wait {self.waittime}s before closing")
             time.sleep(self.waittime)
-        #如果希望使用现货交易，需要把market_close换成普通的order，因为 reduce_only=True 这个参数不适用于现货交易
-        order_result = self.exchange.market_close(self.COIN,sz=self.buyamount)
+        order_result = self.market_close(self.COIN,sz=self.buyamount)
         if order_result["status"] == "ok":
             for status in order_result["response"]["data"]["statuses"]:
                 try:
@@ -55,7 +108,7 @@ def main():
     address, info, exchange = example_utils.setup(constants.MAINNET_API_URL, skip_ws=True)
     exchange.set_referrer("WELANN")
     buysell = buyandsell(
-        address=address, info=info, exchange=exchange, COIN="ETH", buyamount=0.05, waittime=5, isbuy=True
+        address=address, info=info, exchange=exchange, COIN="PURR/USDC", buyamount=1, waittime=5, waittime=5, isbuy=True
     )
     while True:
         buysell.order()
