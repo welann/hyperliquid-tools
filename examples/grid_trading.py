ac0d9552800a5e3706cebfea17c7c2d37c586bc5
welann <wzcing@outlook.com>
2024-04-26 15:20:04 +0800

    增加了新功能：在持有现货时，可以在当前价格上方的价格开卖单卖出

--- examples/grid_trading.py
+++ examples/grid_trading.py
@@ -21,6 +21,7 @@ class grid:
         gridmin=0.06,
         tp=0.01,
         eachgridamount=100,
+        hasspot=False,
     ):
         self.address = address
         self.info = info
@@ -32,6 +33,7 @@ class grid:
         gridmin: 网格下界
         tp: 涨多少就卖掉
         eachgridamount: 每个网格购买的币数量
+        hasspot: 如果为True,则会在当前价格上方放现货卖单,所以需要确保钱包中有现货
         """
         self.COIN = COIN
         self.gridnum = gridnum
@@ -39,6 +41,7 @@ class grid:
         self.gridmin = gridmin
         self.tp = tp
         self.eachgridamount = eachgridamount
+        self.hasspot = hasspot
 
         self.eachprice = []
         # 格式{"index": i, "oid": 0,"activated":False}
@@ -114,6 +117,7 @@ class grid:
         logging.info(f"check sell order: {self.sell_orders}")
         for sell_order in self.sell_orders:
             order_status = self.info.query_order_by_oid(self.address, sell_order["oid"])
+            logging.info("Order status by oid:", order_status)
             logging.info(order_status.get("status"))
             status = order_status.get("status")
             if status != "unknownOid":
@@ -151,10 +155,127 @@ class grid:
                         )
                         self.buy_orders.append({"index": i, "oid": buy_oid, "activated": True})
 
+    def hasspot_compute(self):
+        self.exchange.set_referrer("WELANN")
+        pricestep = (self.gridmax - self.gridmin) / self.gridnum
+        # 不同币种的精度不一样，出现问题可能需要调整
+        # 比如btc的价格是整数
+        for i in range(self.gridnum):
+            price = self.gridmin + i * pricestep
+            self.eachprice.append(round(float(f"{price:.5g}"), 6))
+        logger.info(f"each grid's price: {self.eachprice}")
+        logger.info(f"each grid buy: {self.eachgridamount} {self.COIN}")
+
+        # 初始化网格订单
+        for i in range(self.gridnum):
+
+            # 在当前价格上方开卖单
+            midprice = float(self.info.all_mids()[self.COIN][:-1])
+            if self.eachprice[i] > midprice:
+                order_result = self.exchange.order(
+                    self.COIN, False, self.eachgridamount, self.eachprice[i] + self.tp, {"limit": {"tif": "Gtc"}}
+                )
+                logger.info(f"SELL: init the {i} th grid order : {order_result}")
+
+                if order_result.get("status") == "ok":
+                    buy_oid = (
+                        order_result["response"]["data"]["statuses"][0]["resting"]["oid"]
+                        if order_result["response"]["data"]["statuses"][0].get("resting")
+                        else order_result["response"]["data"]["statuses"][0]["filled"]["oid"]
+                    )
+                    self.sell_orders.append({"index": i, "oid": buy_oid})
+            else:
+                # 在当前价格下方开买单
+                order_result = self.exchange.order(
+                    self.COIN, True, self.eachgridamount, self.eachprice[i], {"limit": {"tif": "Gtc"}}
+                )
+                logger.info(f"BUY: init the {i} th grid order: {order_result}")
+    
+                if order_result.get("status") == "ok":
+                    buy_oid = (
+                        order_result["response"]["data"]["statuses"][0]["resting"]["oid"]
+                        if order_result["response"]["data"]["statuses"][0].get("resting")
+                        else order_result["response"]["data"]["statuses"][0]["filled"]["oid"]
+                    )
+                    self.buy_orders.append({"index": i, "oid": buy_oid})
+
+        logger.info(f"inital buy orders: {self.buy_orders}")
+        logger.info(f"inital sell orders: {self.sell_orders}")
+
+    def hasspot_check_buy_order(self):
+        logging.info(f"check buy order: {self.buy_orders}")
+        for buy_order in self.buy_orders:
+            order_status = self.info.query_order_by_oid(self.address, buy_order["oid"])
+            logging.info("Order status by oid:", order_status)
+            logging.info(order_status.get("status"))
+            status = order_status.get("status")
+            if status != "unknownOid":
+                # 如果此订单已成交
+                if order_status["order"].get("status") == "filled":
+                    logging.info(f"buy order: {buy_order} filled")
+                    # 下个卖单
+                    sell_price = self.eachprice[buy_order["index"]] + self.tp
+                    sell_order_result = self.exchange.order(
+                        self.COIN, False, self.eachgridamount, sell_price, {"limit": {"tif": "Gtc"}}
+                    )
+                    # {'status': 'ok', 'response': {'type': 'order', 'data': {'statuses': [{'resting': {'oid': 7701703031}}]}}}
+                    if sell_order_result.get("status") == "ok":
+                        sell_oid = (
+                            sell_order_result["response"]["data"]["statuses"][0]["resting"]["oid"]
+                            if sell_order_result["response"]["data"]["statuses"][0].get("resting")
+                            else sell_order_result["response"]["data"]["statuses"][0]["filled"]["oid"]
+                        )
+                        # 保存卖单
+                        self.sell_orders.append({"index": buy_order["index"], "oid": sell_oid})
+                        # 删除这个订单
+                        self.buy_orders.remove(buy_order)
+
+    def hasspot_check_sell_order(self):
+        logging.info(f"check sell order: {self.sell_orders}")
+        for sell_order in self.sell_orders:
+            order_status = self.info.query_order_by_oid(self.address, sell_order["oid"])
+            logging.info("Order status by oid:", order_status)
+            logging.info(order_status.get("status"))
+            status = order_status.get("status")
+            if status != "unknownOid":
+                # 如果此订单已成交
+                if order_status["order"].get("status") == "filled":
+                    logging.info(f"sell order: {sell_order} filled")
+                    # 下个买单
+                    buy_order_result = self.exchange.order(
+                        self.COIN,
+                        True,
+                        self.eachgridamount,
+                        self.eachprice[sell_order["index"]],
+                        {"limit": {"tif": "Gtc"}},
+                    )
+                    if buy_order_result.get("status") == "ok":
+                        buy_oid = (
+                            buy_order_result["response"]["data"]["statuses"][0]["resting"]["oid"]
+                            if buy_order_result["response"]["data"]["statuses"][0].get("resting")
+                            else buy_order_result["response"]["data"]["statuses"][0]["filled"]["oid"]
+                        )
+                    self.buy_orders.append({"index": sell_order["index"], "oid": buy_oid})
+                    self.sell_orders.remove(sell_order)
+
+    def check_compute(self):
+        if self.hasspot:
+            self.hasspot_compute()
+        else:
+            self.compute()
+            
     def trader(self):
-        self.check_buy_order()
-        self.check_sell_order()
-        self.check_grid()
+        if self.hasspot:
+            self.hasspot_check_buy_order()
+            self.hasspot_check_sell_order()
+
+        else:
+            self.check_buy_order()
+            self.check_sell_order()
+            self.check_grid()
+            
+        time.sleep(1)
+        
 
 
 def main():
@@ -170,6 +291,7 @@ def main():
         gridmin=0.14,
         tp=0.015,
         eachgridamount=70,
+        hasspot=False
     )
     # trading = grid(
     #     address=address,
@@ -182,7 +304,7 @@ def main():
     #     tp=1,
     #     eachgridamount=0.07,
     # )
-    trading.compute()
+    trading.check_compute()
     while True:
         trading.trader()
         time.sleep(1)
